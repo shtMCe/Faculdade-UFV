9fb8dbc530b0c2cddfdaf57ddb0196a0b8ddea34
Maria Cecília (Ceci) Gonçalves <rosagoncalvesmariacecilia15@gmail.com>
2026-09-03 22:28:03 -0300

    Continuação "Ponteiros1.c"
    Primeiro contato com ponteiros em linguagem C

--- Ponteiros1.c
+++ Ponteiros1.c
@@ -14,6 +14,21 @@ int main (){
     printf("%d \n", *p); // conteúdo do endereço de memória armazenado em 'p'
     
     printf("%d \n", p); // endereço de memória do ponteiro 'p'
+    
+    scanf("%d", &*p); //testando scanf
+    
 
+    //testando condicional com ponteiros
+    if (*p >= 18) {
+        printf("Maior de idade\n");
+    }
+    else {
+        printf("Menor de idade\n");
+    }
+    
+    int **p2 = &p; // ponteiro para ponteiro
+    
+    printf("%d", **p2);
+    
     return 0;
 }
